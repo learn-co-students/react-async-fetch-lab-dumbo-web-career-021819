7b940631960f03edd0eeab54a77e2737e378d631
campbelllssoup <matjocampbell@gmail.com>
2019-04-25 20:13:18 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,36 @@
 // create your App component here
+import React from 'react'
+
+class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      astros: []
+    }
+  }
+
+  generateAstroNames = () => {
+    return this.state.astros.map( ( astro, index) => {
+      return <p key={index} id={index}>{astro.name}</p>
+    })
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(res => res.json() )
+    .then(astros => {
+      this.setState({ astros: astros.people })
+    })
+  }
+
+  render() {
+    return (
+      <main>
+        {this.generateAstroNames()}
+      </main>
+    )
+  }
+}
+
+export default App
